26f18b8172191f7b65cd412d04befc10a2d26424
admin <admin@nightonly.com>
2021-05-20 09:43:59 +0900

    WARN  Though the "loose" option was set to "false" in your @babel/preset-env config

--- nuxt.config.js
+++ nuxt.config.js
@@ -79,5 +79,10 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    babel: {
+      plugins: [
+        ['@babel/plugin-proposal-private-methods', { loose: true }]
+      ]
+    }
   }
 }
